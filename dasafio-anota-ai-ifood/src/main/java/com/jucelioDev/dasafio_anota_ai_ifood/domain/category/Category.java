c6d91dafe39c2b7104e71000b1c6c2eec6c0219e
JUCELIO <juceliocoelho2022@gmail.com>
2024-07-25 11:19:26 -0300

    Create CategoryDTO.java

--- dasafio-anota-ai-ifood/src/main/java/com/jucelioDev/dasafio_anota_ai_ifood/domain/category/Category.java
+++ dasafio-anota-ai-ifood/src/main/java/com/jucelioDev/dasafio_anota_ai_ifood/domain/category/Category.java
@@ -1,15 +1,18 @@
 package com.jucelioDev.dasafio_anota_ai_ifood.domain.category;
 
 
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.json.JSONObject;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document(collation = "categories")
+@Document(collection = "categories")
 @Getter
 @Setter
+@EqualsAndHashCode
 @NoArgsConstructor
 public class Category {
     @Id
@@ -17,4 +20,31 @@ public class Category {
     private String title;
     private String description;
     private String ownerId;
+
+    public Category(CategoryDTO categoryDTO){
+        this.title = categoryDTO.title();
+        this.description = categoryDTO.description();
+        this.ownerId = categoryDTO.ownerId();
+    }
+
+    @Override
+    public String toString(){
+        JSONObject json = new JSONObject();
+        json.put("id", id);
+        json.put("title", title);
+        json.put("description", description);
+        json.put("ownerId", ownerId);
+        json.put("type", "categoria");
+
+        return json.toString();
+    }
+
+    public String deleteToString(){
+        JSONObject json = new JSONObject();
+        json.put("id", this.id);
+        json.put("ownerId", this.ownerId);
+        json.put("type", "delete-categoria");
+
+        return json.toString();
+    }
 }
---
+++ dasafio-anota-ai-ifood/src/main/java/com/jucelioDev/dasafio_anota_ai_ifood/domain/category/CategoryDTO.java
@@ -0,0 +1,4 @@
+package com.jucelioDev.dasafio_anota_ai_ifood.domain.category;
+
+public record CategoryDTO(String title, String description, String ownerId) {
+}
